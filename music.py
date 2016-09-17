49099fa84d450132f30ef3d58ce66362f881dc08
Edward Lee <edlee303@gmail.com>
2016-09-17 23:26:10 +0200

    Extract album IDs

--- music.py
+++ music.py
@@ -1,10 +1,18 @@
 from gmusicapi import Mobileclient
+from getpass import getpass
 
-print 'Enter username' 
-username = raw_input()
-
-print 'Enter password' 
-password = raw_input()
+username = raw_input('Username:')
+password = getpass()
 
 api = Mobileclient()
 api.login(username, password, Mobileclient.FROM_MAC_ADDRESS)
+
+library = api.get_all_songs()
+
+albumIds = []
+
+for i in library:
+	if 'albumId' in i.keys():
+		albumIds.append(i['albumId'])
+
+albumIds = list(set(albumIds))
